edf694a678730247934515af1938a3713dc19bdd
Dima Lunich <lunich@ukr.net>
2011-03-29 02:00:47 +0300

    YamlConfig class added

--- lib/yaml_config.rb
+++ lib/yaml_config.rb
@@ -1,12 +1,3 @@
 require "yaml_config/null_property"
+require "yaml_config/yaml_config"
 
-class YamlConfig
-  def initialize(filename, options = {})
-    @root = options.delete(:root).to_s
-    @yaml = File.exists?(filename) ? YAML.load_file(filename) : YAML.load(filename)
-    raise NameError.new("Root :#{@root} doesn't exist in given YAML") unless @root.empty? || @yaml.has_key?(@root)
-  end
-  def get(key)
-    (@root.empty? ? @yaml[key.to_s] : @yaml[@root][key.to_s]) || NullProperty.new
-  end
-end
